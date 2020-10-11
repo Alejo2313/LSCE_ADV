658bb5aa404e06fb3f8aae07e551e420f0641461
Alejandro Gomez Molina <a.gomezm@alumnos.upm.es>
2020-10-11 22:23:13 +0200

    DMA V2, No tested

--- LCSE_PROJ/LCSE_PROJ.srcs/sources_1/new/DMA.vhd
+++ LCSE_PROJ/LCSE_PROJ.srcs/sources_1/new/DMA.vhd
@@ -58,10 +58,7 @@ architecture Behavioral of DMA is
 
 type DMA_STATE_T is (IDLE, FETCH, TAKE_BUS, WRITE);
 
-signal rxCounter, rxCounter_n : UNSIGNED(1 downto 0);
-signal txCounter, txCounter_n : UNSIGNED(0 downto 0);
 
-signal mode_reg, mode_reg_n   : STD_LOGIC;  -- Operation mode: '1' RAM to RS232 '0' RS232 to RAM
 -- DMA State regd
 signal DMA_state_reg, DMA_state_reg_n   : DMA_STATE_T;
 
@@ -75,6 +72,11 @@ signal DMA_RQ_reg, DMA_RQ_reg_n         : STD_LOGIC;
 signal READY_reg, READY_reg_n           : STD_LOGIC;
 signal Address_reg, Address_reg_n       : STD_LOGIC_VECTOR (7 downto 0);
 signal Databus_reg, Databus_reg_n       : STD_LOGIC_VECTOR (7 downto 0);
+
+
+signal rxCounter, rxCounter_n           : UNSIGNED(1 downto 0);
+signal txCounter, txCounter_n           : STD_LOGIC;
+signal mode_reg, mode_reg_n             : STD_LOGIC;  -- Operation mode: '1' RAM to RS232 '0' RS232 to RAM
 
 
 begin
@@ -92,154 +94,156 @@ begin
         OE_reg          <= '0';
         DMA_RQ_reg      <= '0';
         READY_reg       <= '0';
-        txCounter       <= (others => '0');
+        txCounter       <= '0';
         rxCounter       <= (others => '0');
         mode_reg        <= '0';
+        Address_reg     <= (others => 'Z');
+        Databus_reg     <= (others => 'Z');
         
     elsif ( Clk = '1' and Clk'event) then
         DMA_state_reg   <= DMA_state_reg_n;
         
-        mode_reg        <= mode_reg_n;
         Data_Read_reg   <= Data_Read_reg_n;
         Valid_D_reg     <= Valid_D_reg_n;
         TX_Data_reg     <= TX_Data_reg_n;
         Write_en_reg    <= Write_en_reg_n;
         OE_reg          <= OE_reg_n;
         DMA_RQ_reg      <= DMA_RQ_reg_n;
+        READY_reg       <= READY_reg_n;
         txCounter       <= txCounter_n;
         rxCounter       <= rxCounter_n;
-        READY_reg       <= READY_reg_n;
+        mode_reg        <= mode_reg_n;
+        Address_reg     <= Address_reg_n;
+        Databus_reg     <= Databus_reg_n;
     
     end if;
-
-
 end process;
 
-NSL: process(RX_Empty, Send_comm, DMA_state_reg, TX_RDY, DMA_ACK) is
 
+NSL2: process ( DMA_state_reg, Send_comm, rxCounter, TX_RDY, RX_Empty ) is
 begin
-    case DMA_state_reg is 
-        when IDLE       =>                                      -- System reday. Waiting for events 
-            -- Check if data available in FIFO   
-            if ( RX_empty = '0' ) then                          
-                DMA_state_reg <= FETCH;
-                
-            -- Look for send request    
-            elsif ( Send_comm = '1' or txCounter = 1 ) then
-                DMA_state_reg <= TAKE_BUS;
-            end if;
-        
-        when FETCH =>
-            -- If it's a MEM -> RS232 operation 
-            if ( mode_reg = '1' ) then
-                if ( DMA_ACK = '1' ) then       -- Wait for bus access
-                    DMA_state_reg <= WRITE;     -- Go to writr
-                end if;
-                
-            else
-                DMA_state_reg <= TAKE_BUS;     
+
+    case DMA_state_reg is
+        when IDLE =>
+            --     sendComm event   or  RX data       or  second TX byte 
+            if ( Send_comm = '1' or RX_Empty = '1' or ( txCounter = '1' and TX_RDY = '1' ) ) then
+                DMA_state_reg_n <= FETCH;
             end if;
             
-        when TAKE_BUS =>
-            if ( mode_reg = '1' ) then
-                DMA_state_reg <= FETCH;
-            else
-                DMA_state_reg <= WRITE;     
+       when FETCH =>   -- Fetch data from source
+            if ( DMA_ACK = '1' ) then  -- Wait for bus access
+                 DMA_state_reg_n <= WRITE;
             end if;
             
-        when WRITE =>
-           if ( mode_reg = '1' ) then
-                if ( TX_RDY = '1' ) then        
-                    DMA_state_reg <= IDLE; 
-                end if;  
-           else
-                if ( DMA_ACK = '1' ) then
-                    DMA_state_reg <= IDLE;
+        when WRITE =>  -- Load data to destination
+            if ( mode_reg = '0' ) then  -- If TX mode, send one byte and return to idle (Non-blocking mode)
+                if (TX_RDY = '1' ) then
+                    DMA_state_reg_n <= IDLE;
                 end if;
-           end if;
+            else
+                if ( rxCounter = 3 ) then -- if RX, send all bytes and return to idle (burst mode)
+                    DMA_state_reg_n <= IDLE;
+                else
+                    DMA_state_reg_n <= FETCH;
+                end if;
+            end if;
+                  
     end case;
+
+
 end process;
 
-
-
-LOGIC: process ( DMA_state_reg, DMA_ACK ) is
+LP: process ( DMA_state_reg, Send_comm, txCounter, TX_RDY, RX_Empty, DMA_ACK) is
 
 begin
-    READY_reg_n     <= '0';
-    DMA_RQ_reg_n    <= DMA_RQ_reg;
-    
-    Valid_D_reg_n   <= '0';
-    Data_Read_reg_n <= '0';
-    mode_reg_n      <= mode_reg;
-    
-    Address_reg_n   <= (others => 'Z' );
-    Databus_reg_n   <= (others => 'Z' );
+    -- Set bus lines to high impedance 
+    Address_reg_n   <= (others => 'Z');
+    databus_reg_n   <= (others => 'Z');
     Write_en_reg_n  <= 'Z';
     OE_reg_n        <= 'Z';
     
+    -- Set default values, avoid latches 
+    DMA_RQ_reg_n    <= '0';
+    READY_reg_n     <= '0';
+    mode_reg_n      <= '0';
+    DMA_RQ_reg_n    <= '0';   
+    Data_Read_reg_n <= '0'; 
+    Valid_D_reg_n   <= '0';
+    TX_Data_reg_n   <= TX_Data_reg;
+    txCounter_n     <= txCounter;
+    rxCounter_n     <= rxCounter;
     
-    case DMA_state_reg is 
-        when IDLE       =>                                      -- System reday. Waiting for events 
-            READY_reg_n <= '1';
-            -- Check if data available in FIFO   
-            if ( RX_empty = '0' ) then                          
-                mode_reg_n <= '0';
-                READY_reg_n <= '0';
-            -- Look for send request    
-            elsif ( Send_comm = '1' or txCounter = 1 ) then
-                mode_reg_n <= '1';
-                READY_reg_n <= '0';
-            end if;
+    case DMA_state_reg is
+        when IDLE =>
         
-        when FETCH =>
-            if ( mode_reg = '1' ) then
-                if ( DMA_ACK = '1' ) then   -- Fetch data from RAM   
-                    DMA_RQ_reg_n   <= '0';
-                    Address_reg_n  <= STD_LOGIC_VECTOR( DMA_TX_MSB + txCounter );
-                    OE_reg_n       <= '1';
-                    txCounter_n <= txCounter + 1;
-                end if;
-                
-            else
-                Data_Read_reg_n <= '1';     -- Request FIFO data 
+            READY_reg_n     <= '1';     -- DMA Ready
+            DMA_RQ_reg_n <= '0';     -- Clean request flag
+            -- SendComm event
+            if ( Send_comm = '1'  or ( txCounter = '1' and TX_RDY = '1' ) ) then
+                mode_reg_n   <= '0';   -- Mem to peri mode
+                DMA_RQ_reg_n <= '1';   -- Request bus
+           
+            -- RX FIFO no empty
+            elsif ( RX_Empty = '0' ) then
+                mode_reg_n    <= '1';    -- Peri to mem mode
+                DMA_RQ_reg_n  <= '1';   -- Request bus
             end if;
             
-            
-        when TAKE_BUS =>
-            DMA_RQ_reg_n <= '1';
+       when FETCH =>
+            if ( DMA_ACK = '1' ) then
+                if ( mode_reg = '0' ) then
+                    if ( txCounter = '0' ) then
+                        Address_reg_n <= STD_LOGIC_VECTOR(DMA_TX_MSB);
+                        OE_reg_n <= '1';
+                        txCounter_n <= '1';
+                    else 
+                        Address_reg_n <= STD_LOGIC_VECTOR(DMA_TX_LSB);
+                        OE_reg_n <= '1';
+                        txCounter_n <= '0';
+                    end if; 
+                else
+                    Data_Read_reg_n <= '1';
+                    Write_en_reg_n  <= '0';
+                end if;
+            end if;
             
         when WRITE =>
-           if ( mode_reg = '1' ) then
-                  if ( TX_RDY = '1' ) then
+            if ( mode_reg = '0' ) then
+                OE_reg_n <= '0';
+                DMA_RQ_reg_n   <= '0';
+                
+                if ( TX_RDY = '1' ) then
+                    TX_Data_reg   <= databus;
                     Valid_D_reg_n <= '1';
-                    TX_Data_reg_n <= databus;
-                  end if;
-           else
-                if ( DMA_ACK = '1' ) then
-                    DMA_RQ_reg_n   <= '0';
-                    Address_reg_n  <= STD_LOGIC_VECTOR( DMA_TX_MSB + rxCounter );
-                    Write_en_reg_n <= '1';
-                    Databus_reg_n  <= RCVD_Data;
-                    
-                    if (rxCounter = 2 ) then
-                         rxCounter_n <= (others => '0');
-                    else
-                        rxCounter_n  <= rxCounter + 1;  
-                    end if;
                 end if;
-           end if;
-    end case;
-        
+            else
+            
+                if ( rxCounter = 3 ) then
+                    Address_reg_n <= STD_LOGIC_VECTOR(NEW_INST);
+                    Write_en_reg_n  <= '1';
+                    databus_reg_n <= X"FF";
+                    rxCounter_n   <= (others => '0');
+                else
+                    Address_reg_n <= STD_LOGIC_VECTOR(DMA_RX_MSB + rxCounter);
+                    Write_en_reg  <= '1';
+                    databus_reg_n <= RCVD_Data;
+                    rxCounter_n   <= rxCounter + 1;
+                end if;
+            end if;                     
+    end case;   
 
 end process;
 
+-- Output
 Data_Read   <= Data_Read_reg;
 Valid_D     <= Valid_D_reg;
 TX_Data     <= TX_Data_reg;
 Write_en    <= Write_en_reg;
 OE          <= OE_reg;
 DMA_RQ      <= DMA_RQ_reg;
-
+READY       <= READY_reg;
+Address     <= Address_reg;
+Databus     <= Databus_reg;
 
 
 
