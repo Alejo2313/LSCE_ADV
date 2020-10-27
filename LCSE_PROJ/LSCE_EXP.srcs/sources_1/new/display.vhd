708813c4c956adb10cef9c3ed850b1b82694b1b6
lfVelez <36552876+lfVelez@users.noreply.github.com>
2020-10-27 01:39:07 +0100

    Changed anode output and registered display input

--- LCSE_PROJ/LSCE_EXP.srcs/sources_1/new/display.vhd
+++ LCSE_PROJ/LSCE_EXP.srcs/sources_1/new/display.vhd
@@ -42,7 +42,7 @@ entity display is
            WE_s : in STD_LOGIC;
            RE_s : in STD_LOGIC;
            out_display : out STD_LOGIC_VECTOR(7 downto 0);
-           anode : out STD_LOGIC_VECTOR(2 downto 0));
+           anode : out STD_LOGIC_VECTOR(7 downto 0));
 end display;
 
 architecture Behavioral of display is
@@ -52,10 +52,11 @@ architecture Behavioral of display is
     signal mem_r,mem_n : dev_mem_8;
     signal outBus_r : STD_LOGIC_VECTOR(7 downto 0);
     signal outDisp_r, outDisp_n : STD_LOGIC_VECTOR(7 downto 0);
-    
-    signal cnt_r, cnt_n : UNSIGNED(log2c(400000)-1 downto 0);    
+    signal anode_r,anode_n : STD_LOGIC_VECTOR(7 downto 0);
+    signal cnt_r, cnt_n : UNSIGNED(log2c(50000)-1 downto 0);    
     signal index_r, index_n     : UNSIGNED(log2c(8) -1 downto 0);
     
+    signal d_r, d_n : STD_LOGIC_VECTOR (3 downto 0);  
     --Components
     component decod7s 
         Port ( D : in  STD_LOGIC_VECTOR (3 downto 0);					
@@ -67,7 +68,7 @@ architecture Behavioral of display is
 begin
 
     decoder: decod7s 
-        port map ( D => D,
+        port map ( D => d_r,
                    S => S);
 
  FF: process (Clk, Reset) is
@@ -78,26 +79,75 @@ begin
         outDisp_r <= "00000001";
         index_r <= (others => '0');
         cnt_r <= (others => '0');
+        anode_r <= (others => '1');
+        d_r <= (others => '0');
     elsif (Clk'event and Clk = '1') then
         mem_r <= mem_n; 
         outDisp_r <= outDisp_n; 
         cnt_r <= cnt_n;
         index_r <= index_n;
+        anode_r <= anode_n;
+        d_r <= d_n;
     end if;
  end process;
  
-next_state: process(Address_s, inBus_s,WE_s, RE_s, mem_r,cnt_r,index_r, outDisp_r,S) is
+process(Address_s, inBus_s,WE_s, RE_s, mem_r,cnt_r,index_r,D, outDisp_r,S,anode_r,d_r,D) is
 begin
     outBus_r <= (others => 'Z');
     mem_n <= mem_r;
     cnt_n <= cnt_r + 1;
     index_n <= index_r;
     outDisp_n <= outDisp_r;
+    anode_n <= anode_r;
+    d_n <= d_r;
     
     if (TO_INTEGER(cnt_r) = 400) then
         cnt_n <= (others => '0');        
         index_n <= index_r + 1;
         outDisp_n <= S;  
+        anode_n <= (others => '1');
+        
+        if ((mem_r(0)(7) and not(mem_r(1)(TO_INTEGER(index_r)))) = '1') then
+            case index_r is
+                when "000" => 
+                    anode_n(0) <= '0';
+                    d_n <= mem_r(TO_INTEGER(DISPLAY_01-DISPLAY_BASE))(3 downto 0);
+                     
+                when "001" =>
+                    anode_n(1) <= '0';
+                    d_n <= mem_r(TO_INTEGER(DISPLAY_01-DISPLAY_BASE))(7 downto 4);
+                     
+                when "010" =>
+                    anode_n(2) <= '0'; 
+                    d_n <= mem_r(TO_INTEGER(DISPLAY_23-DISPLAY_BASE))(3 downto 0);
+                     
+                when "011" =>
+                    anode_n(3) <= '0'; 
+                    d_n <= mem_r(TO_INTEGER(DISPLAY_23-DISPLAY_BASE))(7 downto 4);
+                    
+                when "100" =>
+                    anode_n(4) <= '0'; 
+                    d_n <= mem_r(TO_INTEGER(DISPLAY_45-DISPLAY_BASE))(3 downto 0);
+                     
+                when "101" =>
+                    anode_n(5) <= '0'; 
+                    d_n <= mem_r(TO_INTEGER(DISPLAY_45-DISPLAY_BASE))(7 downto 4);
+                     
+                when "110" =>
+                    anode_n(6) <= '0'; 
+                    d_n <= mem_r(TO_INTEGER(DISPLAY_67-DISPLAY_BASE))(3 downto 0);
+                    
+                when "111" =>
+                    anode_n(7) <= '0';
+                    d_n <= mem_r(TO_INTEGER(DISPLAY_67-DISPLAY_BASE))(7 downto 4);
+                     
+                when others =>
+                    d_n <= "0000";
+                            
+            end case;         
+        else
+            d_n <= "0000";
+        end if;       
     end if;
     
     if (WE_s = '1') then
@@ -105,24 +155,27 @@ begin
     elsif (RE_s = '1') then
         outBus_r <= mem_r(TO_INTEGER(UNSIGNED(Address_s) - DISPLAY_BASE));
     end if; 
-end process;
-
-output: process(index_r,mem_r,D,outDisp_r,S) is
-begin
-    if ((mem_r(TO_INTEGER(DISPLAY_EN-DISPLAY_BASE))(7) = '1') and mem_r(TO_INTEGER(DISPLAY_ANODE-DISPLAY_BASE))(TO_INTEGER(index_r)) = '0') then
-        if (index_r(0) = '0') then
-            D <= mem_r(2 + TO_INTEGER('0' & index_r(2 downto 1)))(3 downto 0) ;
-        else
-            D <= mem_r(2 +TO_INTEGER('0' & index_r(2 downto 1)))(7 downto 4) ;
-        end if;
-    else
-        D<= "0000";    
-    end if;
     
+
 end process;
 
+--output: process(index_r,mem_r,D,outDisp_r,S) is
+--begin
+--    if ((mem_r(TO_INTEGER(DISPLAY_EN-DISPLAY_BASE))(7) = '1') and mem_r(TO_INTEGER(DISPLAY_ANODE-DISPLAY_BASE))(TO_INTEGER(index_r)) = '0') then
+--        if (index_r(0) = '0') then
+--            D <= mem_r(2 + TO_INTEGER('0' & index_r(2 downto 1)))(3 downto 0) ;
+--        else
+--            D <= mem_r(2 +TO_INTEGER('0' & index_r(2 downto 1)))(7 downto 4) ;
+--        end if;
+--    else
+--        D<= "0000";    
+--    end if;
+    
+--end process;
+anode <= anode_r;  
 outBus_s <= outBus_r;  
 out_display <= outDisp_r; 
-anode <= std_logic_vector(index_r) when (mem_r(TO_INTEGER(DISPLAY_EN-DISPLAY_BASE))(7) = '1') else
-         (others => '0');    
+--anode <= std_logic_vector(index_r) when (mem_r(TO_INTEGER(DISPLAY_EN-DISPLAY_BASE))(7) = '1') else
+--         (others => '0');  
+         
 end Behavioral;
